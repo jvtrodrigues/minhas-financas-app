cafe363af2541e8001d89ad1d5a18e1def1d9bce
José Rodrigues <josehgunner@gmail.com>
2020-03-28 21:10:40 -0300

    Utilizando o serviço da API de usuários no componente de Login

---
+++ src/main/rotas.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Route, Switch, HashRouter} from 'react-router-dom' 
+import Login from '../views/login'
+import CadastroUsuario from '../views/cadastroUsuario'
+import Home from '../views/home'
+function Rotas () {
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/cadastro-usuarios" component={CadastroUsuario}/>
+                <Route path="/home" component={Home}/>
+            </Switch>
+        </HashRouter>
+    )
+}
+
+export default Rotas
--- src/views/login.js
+++ src/views/login.js
@@ -1,54 +1,77 @@
 import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
+import {withRouter} from 'react-router-dom'
+import UsuarioService from '../app/service/usuarioService'
 
 class Login extends React.Component {
 
     state = {
         email: '',
-        senha: ''
+        senha: '',
+        mensagemErro: null
+    }
+
+    constructor() {
+        super()
+        this.service = new UsuarioService()
     }
 
     entrar = () => {
-        console.log('email: ', this.state.email)
-        console.log('senha: ', this.state.senha)
+        this.service.autenticar({
+            email: this.state.email,
+            senha: this.state.senha
+        }).then(response => {
+            localStorage.setItem('_usuario_logado', JSON.stringify(response.data))
+            this.props.history.push('/home')
+        }).catch(erro => {
+            console.log('entrou no erro')
+            this.setState({mensagemErro: erro.response.data})
+        })
+
+        console.log('executado a requisição')
+    }
+
+    prepareCadastrar = () => {
+        this.props.history.push('/cadastro-usuarios')
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={{position: 'relative', left: '300px'}}>
-                       <div className="bs-docs-section">
-                         <Card title="Login">
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <span>{this.state.mensagemErro}</span>
+                            </div>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="bs-component">
                                         <fieldset>
                                             <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                <input type="email" 
-                                                value={this.state.email}
-                                                onChange={e => this.setState({email: e.target.value})}
-                                                className="form-control" 
-                                                id="exampleInputEmail1" 
-                                                aria-describedby="emailHelp" 
-                                                placeholder="Digite o Email"/>
+                                                <input type="email"
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email" />
                                             </FormGroup>
-                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1"> 
-                                            <input type="password" 
-                                            value={this.state.senha}
-                                            onChange={e => this.setState({senha: e.target.value})}
-                                            className="form-control" 
-                                            id="exampleInputPassword1" placeholder="Password"/>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1" placeholder="Password" />
                                             </FormGroup>
                                             <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                            <button className="btn btn-danger">Cadastrar</button>
+                                            <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
                                         </fieldset>
                                     </div>
                                 </div>
                             </div>
-                        </Card>  
-                       </div>
+                        </Card>
                     </div>
                 </div>
             </div>
@@ -56,4 +79,4 @@ class Login extends React.Component {
     }
 }
 
-export default Login
+export default withRouter( Login )
